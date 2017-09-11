31ff2fe68ee9ec8784782ea20dcf58b773dfc624
Reuben Palmer <reuben.j.palmer@gmail.com>
2017-09-11 14:21:38 +1200

    demo for push

--- StakeOut/StakeOut/StakeOut.Android/MainActivity.cs
+++ StakeOut/StakeOut/StakeOut.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace StakeOut.Droid
 {
-    [Activity(Label = "StakeOut", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "StakeOut", Icon = "@drawable/sandwich", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
