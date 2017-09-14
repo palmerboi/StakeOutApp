61237bcfbef1a3cdc1f00904b2f6637b13a27417
Reuben Palmer <reuben.j.palmer@gmail.com>
2017-09-14 14:35:50 +1200

    stuff

---
+++ StakeOut/StakeOut/StakeOut/TabbedPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StakeOut
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabbedPage : TabbedPage
+    {
+        public TabbedPage()
+        {
+            InitializeComponent();
+        }
+
+        public static implicit operator Page(TabbedPage v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
