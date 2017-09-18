9d1d99d9b74d55721d249eef0aa635fe5bd9bd68
Reuben Palmer <reuben.j.palmer@gmail.com>
2017-09-18 14:09:00 +1200

    quanchi

--- StakeOut/StakeOut/StakeOut/App.xaml.cs
+++ StakeOut/StakeOut/StakeOut/App.xaml.cs
@@ -13,7 +13,7 @@ namespace StakeOut
         {
             InitializeComponent();
 
-            MainPage = new StakeOut.MainPage();
+            MainPage = new StakeOut.ProfilePage();
         }
 
         protected override void OnStart()
