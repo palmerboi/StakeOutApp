8219db2da8cfff7ffd3b57b893f5e3e046966824
Reuben Palmer <reuben.j.palmer@gmail.com>
2017-10-02 20:28:00 +1300

    added link to database backend

--- StakeOut/StakeOut/StakeOut.Android/MainActivity.cs
+++ StakeOut/StakeOut/StakeOut.Android/MainActivity.cs
@@ -21,6 +21,7 @@ namespace StakeOut.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+            Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
             ImageCircleRenderer.Init();
             LoadApplication(new App());
         }
