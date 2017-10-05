f41fead21afe0d7223fd3f2d6877f9421024da7f
Reuben Palmer <reuben.j.palmer@gmail.com>
2017-10-06 08:54:54 +1300

    new stuff

--- StakeOut/StakeOut/StakeOut/Models/Profile.cs
+++ StakeOut/StakeOut/StakeOut/Models/Profile.cs
@@ -8,6 +8,12 @@ namespace StakeOut.Models
 {
     class Profile
     {
+        [Newtonsoft.Json.JsonProperty("Id")]
+        public string Id { get; set; }
+
+        [Microsoft.WindowsAzure.MobileServices.Version]
+        public string AzureVersion { get; set; }
+
         public string name { get; set; }
         public string email { get; set; }
         public DateTime dob;
---
+++ StakeOut/StakeOut/StakeOut/Services/AzureDataService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+using Microsoft.WindowsAzure.MobileServices.SQLiteStore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StakeOut.Services
+{
+    class AzureDataService
+    {
+        public MobileServiceClient MobileService { get; set; }
+        IMobileServiceSyncTable profileTable;
+    }
+}
